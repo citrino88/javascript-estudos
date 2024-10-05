365321031ac336597d159b49de0408ae0498dbc0
citrino88 <almeida.citrino@gmail.com>
2024-10-05 11:38:18 -0300

    querySelector / innerText / preventDefault/ Math / step

--- cap02/js/ex2_1est.js
+++ cap02/js/ex2_1est.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('form')
-const resp = document.querySelector('h3')
-
+const respModulo = document.querySelector('#modulo')
+//ver comentário em saída
 form.addEventListener("submit", (naoReload) =>{
     // ENTRADA
     const numero1 = form.num1.value
@@ -14,8 +14,15 @@ form.addEventListener("submit", (naoReload) =>{
     const aleatorio = Math.random()
     const arredondamento = Math.round(numero1)
     const raizQuadrada = Math.sqrt(numero1)
-
     // SAÍDA
-    resp.innerText = `Respostas\n${numero1}\n${numero2}\n${absoluto}`
+    respModulo.innerText = `Resposta: ${modulo1}`
+    //como vou usar somente uma vez, então não vou declarar outras constantes
+    document.querySelector('#absoluto').innerText = `Resposta: ${absoluto}`
+    document.querySelector('#arreCima').innerText = `Resposta: ${arredondCima}`
+    document.querySelector('#arreBaixo').innerText = `Resposta: ${arredondBaixo}`
+    document.querySelector('#expoente').innerText = `Resposta: ${expoente}`
+    document.querySelector('#aleatorio').innerText = `Resposta: ${aleatorio}`
+    document.querySelector('#arredonda').innerText = `Resposta: ${arredondamento}`
+    document.querySelector('#raizQuadrada').innerText = `Resposta: ${raizQuadrada}`
     naoReload.preventDefault()
 })
