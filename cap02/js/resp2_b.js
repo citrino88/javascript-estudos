2fdbf36f241f63eb74e27e6ba661946a43dbf4d9
citrino88 <almeida.citrino@gmail.com>
2024-10-07 16:53:00 -0300

    Resolução com Math.ceil (arredondamento para cima)

--- cap02/js/resp2_b.js
+++ cap02/js/resp2_b.js
@@ -1,4 +1,18 @@
 'use strict'
 /* Exercício
-b) Elaborar um programa para um lan house de um aeroporto - O programa deve ler o valor de cada 15 minutos de uso de um computador e o tempo de uso por um cliente em minutos. Informe o valor a ser pago pelo cliente, sabendo que as frações extas de 15 minutos devem ser cobradas de forma integral.
+b) Elaborar um programa para um lan house de um aeroporto - O programa deve ler o valor de cada 15 minutos de uso de um computador e o tempo de uso por um cliente em minutos. Informe o valor a ser pago pelo cliente, sabendo que as frações extras de 15 minutos devem ser cobradas de forma integral.
 */
+
+const frm = document.querySelector('form')
+const resposta = document.querySelector('h3')
+
+frm.addEventListener('submit', (naoReload)=>{
+    //entradas
+    const valor = Number(frm.inValorUso.value)
+    const tempo = Number(frm.inTempo.value)
+    //processamento
+    const pagamento = Math.ceil(tempo/15) * valor
+    //saída
+    resposta.innerText = `Valor a pagar é R$ ${pagamento.toFixed(2)}`
+    naoReload.preventDefault()
+})
