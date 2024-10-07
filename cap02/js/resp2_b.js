8dba45fa5dd669d8a6a99567db01c92ad6db83df
citrino88 <almeida.citrino@gmail.com>
2024-10-07 15:01:14 -0300

    exercício JS Lan House

---
+++ cap02/js/resp2_b.js
@@ -0,0 +1,4 @@
+'use strict'
+/* Exercício
+b) Elaborar umprograma para um lan house de um aeroporto - O programa deve ler o valor de cada 15 minutos de uso de um computador e o tempo de uso por um cliente em minutos. Informe o valor aser pago pelo cliente, sabendo que as frações extasde15 minutos devere sercobradas deforma integral.
+*/
