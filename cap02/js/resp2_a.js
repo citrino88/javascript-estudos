4b14724587b929119efda05ae2a787f519244bd6
citrino88 <almeida.citrino@gmail.com>
2024-10-07 12:09:41 -0300

    Programa Farmácia JS - exercício

---
+++ cap02/js/resp2_a.js
@@ -0,0 +1,4 @@
+'use strict'
+/* Exercício
+
+a) Uma farmácia está com uma promoção - Na compra de duas unidades de um mesmo medicamento, o cliente recebe como desconto os centavos do valor total. Elaborar um programa que leia a descrição e preço de um medicamento. Informe o valor do produto na promoção. */
