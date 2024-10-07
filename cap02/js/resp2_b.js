f9316f76ef2f8d14d7654182aefd165d2f003c1f
citrino88 <almeida.citrino@gmail.com>
2024-10-07 15:20:52 -0300

    html

--- cap02/js/resp2_b.js
+++ cap02/js/resp2_b.js
@@ -1,4 +1,4 @@
 'use strict'
 /* Exercício
-b) Elaborar umprograma para um lan house de um aeroporto - O programa deve ler o valor de cada 15 minutos de uso de um computador e o tempo de uso por um cliente em minutos. Informe o valor aser pago pelo cliente, sabendo que as frações extasde15 minutos devere sercobradas deforma integral.
+b) Elaborar um programa para um lan house de um aeroporto - O programa deve ler o valor de cada 15 minutos de uso de um computador e o tempo de uso por um cliente em minutos. Informe o valor a ser pago pelo cliente, sabendo que as frações extas de 15 minutos devem ser cobradas de forma integral.
 */
