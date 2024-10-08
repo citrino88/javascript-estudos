89b0ce0091b69d01db0b6e796e705a4de6bd7172
citrino88 <almeida.citrino@gmail.com>
2024-10-08 11:48:51 -0300

    outro exemplo

---
+++ cap03/ex3_3.js
@@ -0,0 +1,9 @@
+/* a) Elaborar um programa para uma empresa que leia o salário e o tempo que um funcionário trabalha na empresa. Sabendo que a cada 4 anos (quadriênio) o funcionário recebe um acréscimo de 1% no salário, calcule e informe o número de quadriênios a que o funcionário tem direito e o salário final. */
+const prompt = require("prompt-sync")()
+const salario =  Number(prompt("Salário R$: "))
+const tempo = Number(prompt("Tempo (anos): "))
+const quadrienios = Math.floor(tempo / 4)
+const acrescimo =  salario * quadrienios / 100 // ou * 0.01
+console.log(`Quadriênios: ${quadrienios}`)
+console.log(`Salário Final R$: ${(salario+acrescimo).toFixed(2)}`)
+
